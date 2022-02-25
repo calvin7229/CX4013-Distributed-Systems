06e604e415c025e92670b968566b88c5e3852ad8
shiheng7 <shwong01@hotmail.com>
2022-02-25 23:55:50 +0800

    Added server socket class, though functionality is still untested

---
+++ server/udpserver.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <cstring>
+#include <netinet/in.h>
+#include <sys/time.h> 
+
+class UDPServer {
+    private:
+        int sockfd;         // File descriptor for the socket object
+        int port;           // Port number to listen on
+        size_t clientLen;   // Client's address size in number of bytes
+
+        // Stucture describing an Internet socket address
+        sockaddr_in serverAddr;      // Details related to server's address      
+        sockaddr_in clientAddr;      // Details related to client's address
+
+    public:
+        UDPServer(int port);
+
+        int receive(char* buffer, size_t bufferSize, int timeoutSecs);
+        void send(const char* buffer, size_t bufferSize);
+
+        sockaddr_in getClientAddr();
+        size_t getClientLen();
+};
